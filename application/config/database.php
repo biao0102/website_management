5be9b89e473c6da7881e8257f068830caa4b8d65
zhangbiao <zhangbiao@zhangbiaotekiMacBook-Pro.local>
2015-07-15 19:17:21 +0800

    delete  some important information

--- application/config/database.php
+++ application/config/database.php
@@ -3,9 +3,9 @@
 $active_group = 'database01';
 $active_record = TRUE;
 
-$db['database01']['hostname'] = '10.6.16.67';
+$db['database01']['hostname'] = '';
 $db['database01']['username'] = 'backenduser';
-$db['database01']['password'] = 'backenduser_1024';
+$db['database01']['password'] = '';
 $db['database01']['database'] = 'backend';
 $db['database01']['dbdriver'] = 'mysql';
 $db['database01']['dbprefix'] = '';
@@ -19,9 +19,9 @@ $db['database01']['swap_pre'] = '';
 $db['database01']['autoinit'] = TRUE;
 $db['database01']['stricton'] = FALSE;
 
-$db['database02']['hostname'] = '10.6.16.67';
+$db['database02']['hostname'] = '';
 $db['database02']['username'] = 'cmsuser';
-$db['database02']['password'] = 'cmsuser_1024';
+$db['database02']['password'] = '';
 $db['database02']['database'] = 'cms';
 $db['database02']['dbdriver'] = 'mysql';
 $db['database02']['dbprefix'] = '';
